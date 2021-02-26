6721a1b1e99e65f0cf0b9aedd135b642fc50c9e6
IdaEkmark <79712619+IdaEkmark@users.noreply.github.com>
2021-02-26 17:18:59 +0100

    Add files via upload

---
+++ generate_current_profile_radius_time.py
@@ -0,0 +1,59 @@
+# Importer som inte används atm
+#from DREAM.DREAMOutput import DREAMOutput
+#import DREAM.Settings.Equations.DistributionFunction as DistFunc
+#import DREAM.Settings.Equations.IonSpecies as Ions
+#import DREAM.Settings.Equations.RunawayElectrons as Runaways
+#import DREAM.Settings.Solver as Solver
+#import DREAM.Settings.CollisionHandler as Collisions
+
+###################################################################################################
+import numpy as np
+import sys
+#from scipy import integrate
+from scipy import interpolate
+import matplotlib.pyplot as plt
+
+sys.path.append('../../py/')
+
+from DREAM.DREAMSettings import DREAMSettings
+from DREAM import runiface
+
+def generate_current_profile_fun(I_p,current_profile,radie,tid,DREAM_settings_object):
+    #Skapar DREAM-settings och output för att få conductivity
+    ds_conductivity = DREAMSettings(DREAM_settings_object, chain=False)
+    ds_conductivity.eqsys.E_field.setPrescribedData(0)
+    #ds_conductivity.timestep.setNt(1)
+    ds_conductivity.save('settings_conductivity_run.h5')
+    do_conductivity = runiface(ds_conductivity, 'output_conductivity_run.h5', quiet=False)
+    # Simulerar med de DREAMsettings som vi genererat och skapar ett output-objekt för att finna konduktivitet
+
+    #Bestämmer E-fält som krävs för att få önskad strömprofil
+    conductivity = do_conductivity.other.fluid.conductivity[-1,:]
+    VpVol = do_conductivity.grid.VpVol[:]
+
+    dr = do_conductivity.grid.dr
+    r = do_conductivity.grid.r
+    t = do_conductivity.grid.t
+    #r_f = do_conductivity.grid.r_f
+    inter_cub_current_profile = interpolate.interp2d(radie, tid, current_profile, kind='cubic')
+    j_prof_inter = inter_cub_current_profile(r, t)
+    inter_cub_I_p = interpolate.interp1d(tid, I_p, kind='cubic')
+    I_p_inter = inter_cub_I_p(t)
+
+    E_r_t = []
+    i=0
+    for elements in j_prof_inter:
+        current_profile_integral = sum(elements*VpVol*dr) #np.sum???
+        j_0 = 2 * np.pi * I_p_inter[i] / current_profile_integral
+        E_r = j_0 * elements / conductivity
+        E_r_t.append(E_r)
+        i=i+1
+    E_r_t = np.array(E_r_t)
+
+    #Skapar DREAM-settings och output för att få önskad strömprofil
+    ds_fun = DREAMSettings(DREAM_settings_object,chain=False)
+    ds_fun.eqsys.E_field.setPrescribedData(E_r_t, times=t, radius=r)
+    ds_fun.save('settings_generate_current_profile_fun.h5')
+    do_fun = runiface(ds_fun, 'output_generate_current_profile_fun.h5', quiet=False)
+
+    return ds_fun, do_fun
---
+++ generate_current_profile_test2.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+#
+# This example shows how to set up a simple CODE-like runaway
+# scenario in DREAM. The simulation uses a constant temperature,
+# density and electric field, and generates a runaway current
+# through the electric field acceleration, demonstrating Dreicer generation.
+#
+# Run as
+#
+#   $ ./basic.py
+#   $ ../../build/iface/dreami dream_settings.h5
+#
+# ###################################################################
+
+#Test för att göra en tids-och radieberoende strömprofil med parametrar passande de i de Vries (tror jag)
+#Jag  har ändrat n, T, magnetfält, minor radius, major radius och tMax
+#Jag har använt en konstant strömprofil
+#Jag har valt att I_p ska variera i tiden precis som i de Vries
+#Det har inte fungerat så bra då I_p inte blir som jag angett (trots att E blir bra), samt ökar när jag ökar Np
+#Inte heller n_re verkar öka vilket är tråkigt
+#f_hot ser inte helt orimlig ut iallafall
+
+#Släng bort f_hot, gör ren fluid-modell
+#ds.eqsys.setavalanche se zoom-meddelande
+
+import numpy as np
+import sys
+from scipy import integrate
+import matplotlib.pyplot as plt
+from generate_current_profile_radius_time import generate_current_profile_fun
+
+sys.path.append('../../py/')
+
+from DREAM.DREAMSettings import DREAMSettings
+from DREAM.DREAMOutput import DREAMOutput
+from DREAM import runiface
+import DREAM.Settings.Equations.DistributionFunction as DistFunc
+import DREAM.Settings.Equations.IonSpecies as Ions
+import DREAM.Settings.Equations.RunawayElectrons as Runaways
+import DREAM.Settings.Solver as Solver
+import DREAM.Settings.CollisionHandler as Collisions
+
+#Nu blir det intressant
+e=1.60217662e-19
+c=299792458
+I_re=5e5
+A_c=3
+n_re0=I_re/(e*c*A_c)#j_re/(e*c)
+
+#Taget från examples/runaway/basic.py
+ds = DREAMSettings()
+#ds.collisions.collfreq_type = Collisions.COLLFREQ_TYPE_COMPLETELY_SCREENED
+ds.collisions.collfreq_type = Collisions.COLLFREQ_TYPE_PARTIALLY_SCREENED
+
+# Physical parameters
+E = 6       # Electric field strength (V/m)
+n = 3e18#+n_re0    # Electron density (m^-3)
+T = 80      # Temperature (eV)
+B = 2.4     # Magnetic field
+a = np.sqrt(A_c/np.pi)     # Minor radius
+r_o = 3     # Major radius
+
+# Grid parameters
+pMax = 1    # maximum momentum in units of m_e*c
+Np   = 600  # number of momentum grid points
+Nxi  = 40   # number of pitch grid points
+tMax = 10 # simulation time in seconds
+Nt   = 200   # number of time steps
+
+# Set E_field
+ds.eqsys.E_field.setPrescribedData(E)
+
+# Set temperature
+ds.eqsys.T_cold.setPrescribedData(T)
+
+# Set ions
+ds.eqsys.n_i.addIon(name='D', Z=1, iontype=Ions.IONS_PRESCRIBED_FULLY_IONIZED, n=n)
+
+# Runaway electrons
+ds.eqsys.n_re.setAvalanche(avalanche=Runaways.AVALANCHE_MODE_NEGLECT)
+#ds.eqsys.n_re.setAvalanche(avalanche=Runaways.AVALANCHE_MODE_FLUID_HESSLOW)
+ds.eqsys.n_re.setInitialProfile(density=n_re0)
+#ds.eqsys.n_re.setDreicer(Runaways.DREICER_RATE_DISABLED)
+
+# Hot-tail grid settings
+ds.hottailgrid.setNxi(Nxi)
+ds.hottailgrid.setNp(Np)
+ds.hottailgrid.setPmax(pMax)
+ds.hottailgrid.setEnabled(False)
+#ds.hottailgrid.setBiuniformGrid(psep=0.15,npsep_frac=0.5)
+
+# Set initial hot electron Maxwellian
+#ds.eqsys.f_hot.setInitialProfiles(n0=n, T0=T)
+
+# Set boundary condition type at pMax
+ds.eqsys.f_hot.setBoundaryCondition(DistFunc.BC_PHI_CONST) # extrapolate flux to boundary
+ds.eqsys.f_hot.setBoundaryCondition(DistFunc.BC_F_0) # F=0 outside the boundary
+ds.eqsys.f_hot.setSynchrotronMode(DistFunc.SYNCHROTRON_MODE_NEGLECT)
+
+# Disable runaway grid
+ds.runawaygrid.setEnabled(False)
+
+# Set up radial grid
+ds.radialgrid.setB0(B)
+ds.radialgrid.setMinorRadius(a)
+#ds.radialgrid.setMajorRadius(r_o)
+ds.radialgrid.setWallRadius(a)
+ds.radialgrid.setNr(10)
+
+# Set solver type
+ds.solver.setType(Solver.LINEAR_IMPLICIT) # semi-implicit time stepping
+ds.solver.preconditioner.setEnabled(False)
+
+# include otherquantities to save to output
+ds.other.include('fluid')
+
+# Set time stepper
+ds.timestep.setTmax(tMax)
+ds.timestep.setNt(Nt)
+
+ds.output.setTiming(stdout=True, file=True)
+
+r=np.linspace(0,a,num=10)
+t=np.linspace(0,tMax,num=Nt)
+rr, tt = np.meshgrid(r, t)
+j=1-(rr/a)**2
+I_p=0.9*np.exp(-1/(10*(t+1.697976979769798)))*1e6
+
+#plt.plot(t,I_p)
+#plt.xlim(0, 8.5)
+#plt.ylim(0, 1)
+#plt.show()
+
+ds_ny, do_ny = generate_current_profile_fun(I_p,j,r,t,ds)
+
+#do_ny.eqsys.E_field.plot(r=[0,5,-1])
+#plt.xlim(0, tMax)
+#plt.ylim(0, 0.35)
+#plt.show()
+#do_ny.eqsys.I_p.plot()
+#plt.show()
+#do_ny.eqsys.f_hot.plot(t=[0,5,10,25,50,-1],r=[0])
+#plt.show()
+
+j_tot = do_ny.eqsys.j_tot[:]
+#print(str(j_tot))
+
+m_e=9.10938356e-31
+T_J=T*1.602176565e-19 #Temperatur i joule
+v_th=np.sqrt(T_J/m_e)
+
+xi=np.abs(j_tot/(e*n*v_th))
+
+
+tid=np.linspace(0,tMax,num=Nt+1)
+
+#plt.plot(tid,xi[:,0])
+#plt.show()
+
+do_ny.eqsys.n_tot.plot(r=[0])
+do_ny.eqsys.n_re.plot(r=[0])
+plt.xlim(0.5, tMax)
+#plt.ylim(0, 0.35)
+plt.show()
