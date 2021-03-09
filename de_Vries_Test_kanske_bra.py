8a18383e3de844aa9b529416f5327f2b204506a7
IdaEkmark <79712619+IdaEkmark@users.noreply.github.com>
2021-03-09 10:39:22 +0100

    Update de_Vries_Test_kanske_bra.py

--- de_Vries_Test_kanske_bra.py
+++ de_Vries_Test_kanske_bra.py
@@ -1,31 +1,21 @@
-#!/usr/bin/env python3
-#
-# This example shows how to set up a simple CODE-like runaway
-# scenario in DREAM. The simulation uses a constant temperature,
-# density and electric field, and generates a runaway current
-# through the electric field acceleration, demonstrating Dreicer generation.
-#
-# Run as
-#
-#   $ ./basic.py
-#   $ ../../build/iface/dreami dream_settings.h5
-#
-# ###################################################################
-
 # Test för att reproducera de Vries med parametrar passande de i de Vries (tror jag)
 
 # Jag  har beräknat E(t) på samma sätt som de Vries gör (från I_tot och V_loop) samt satt
 # T_cold och n_cold till en funktion lik den data de Vries använder. Stämmer det att
-# T_cold är termala temperaturen och n_cold är termala elektron-densiteten?
+# T_cold är termala temperaturen och n_cold är termala elektron-densiteten? Ja
+# T_cold är temperaturen för Maxwellianen. Främsta anledning till att evaluera avalanche och collisions etc
+# n_cold är antalet fria elektroner i systemet, alla fria elektroner minus runaways
 
 # Jag får inte samma I_p som de Vries I_tot utan den beter sig lite intressant men I_RE
-# går mot I_p när tiden ökar så det är ju trevligt och kan ganska enkelt förklaras med
-# tanke på att det är olika ekvationer som används i simuleringarna
+# går mot I_p när tiden ökar så det är ju trevligt och kanske kan ganska enkelt förklaras
+# med tanke på att det är olika ekvationer som används i simuleringarna
+# Känslig för densitet och temperatur-utvecklingen
 
 # Skenande-elektron-densiteten ökar också mer och mer och blir större än total-elektron-
 # densiteten efter 150 s vilket inte är super - det vore bra med något som skulle kunna hindra
 # n_re > n_tot, men det spelar egentligen inte jättestor roll då vi bara ska göra simuleringar
 # i ca 10 s
+# Antalet fria elektroner är konstant så om runaways försvinner uppkommer nya kalla elektroner
 
 # Kan vara så att de Vries räknar ut E på ett annat sätt än jag har antagit från det han
 # skrev, eftersom han senare skrev att han tar hänsyn till att Coulomb logaritmen ändras
@@ -33,6 +23,24 @@
 
 # Bytte D0=0.03 mot D0=a**2/18\approx 0.05 men det blev bättre att ha D0=0.03 så jag bytte
 # tillbaka
+
+# Jag har ändrat Z från 1 till 2 i enighet med de Vries. För att få en någorlunda konstant
+# I_p sänkte jag sedan B från 2.4 till 1.15 eftersom det funkade samt vi inte är säkra på vad
+# de Vries har använt
+#
+
+# Till skillnad från de Vries får vi att I_re=I_p efter några sekunder oavsett om
+# I_re(0)=5.5e5 A eller I_re(0)=6.5e5 A (de Vries får att detta endast stämmer för
+# I_re(0)=6.5e5 A)
+
+# Kappa parameter i de Vries, hoppar vissa effekter och löser dem med kappa
+# DREAM hoppar inte dessa effekter
+# Försöka få till den kappa som de Vries använder i vår kod
+# Försöka hitta mer rimliga värden på kappa genom att analysera
+# kappa = E_c_eff/E_c_free other.fluid, magnetfält och plasmats laddning påverkar
+# Väte bas lägger till ytterliggare ett jonslag, Berrylium? Kol? Vi vill inte använda kol.
+# Använd berrylium tror vi. De vries pratar om berrylium.
+# Passa så att elektrondensiteten konstant genom att inte lägga tillför mycket Berryliym eller väte.
 
 import numpy as np
 import sys
@@ -43,14 +51,11 @@ from generate_current_profile import generate_current_profile_fun
 sys.path.append('../../py/')
 
 from DREAM.DREAMSettings import DREAMSettings
-from DREAM.DREAMOutput import DREAMOutput
 from DREAM import runiface
-import DREAM.Settings.Equations.DistributionFunction as DistFunc
 import DREAM.Settings.Equations.IonSpecies as Ions
 import DREAM.Settings.Equations.RunawayElectrons as Runaways
 import DREAM.Settings.Solver as Solver
 import DREAM.Settings.CollisionHandler as Collisions
-#import DREAM.Settings.Equations.ElectricField as ElectricField
 import DREAM.Settings.TransportSettings as Transport
 
 #### Physical parameters ####
@@ -65,12 +70,17 @@ A_c   = 3                   # Plasma cross section [m^2]
 L_tor = 5e-6                # Inductance [H]
 r_0   = 3                   # Major radius [m]
 a     = np.sqrt(A_c/np.pi)  # Minor radius [m]
-B     = 2.4                 # Magnetic field [T]
+B     =2.4                 # Magnetic field [T]
+tau_RE = 18
 A0    = 0                   # Advection [???]
-D0    = 0.03                # Diffusion [m^2/s ?]
+# Skippa advection helt och hållet
+D0    = D0=a/2/tau_RE               # Diffusion [m^2/s ?]
+# Jämförde DREAMs diffusionsterm (lossterm) med de Vries loss-term och fick fram
+# att vi borde använda D0=a/2/tau_RE
+# Enheter blir skumma men vi sätter in en 1a så att allt blir bra :)
 
 # Time and radial parameters
-tMax = 10    # Simulation time [s]
+tMax = 8.5    # Simulation time [s]
 Nt   = 10000  # Number of time steps
 Nr   = 1      # Number of radial steps
 
@@ -83,7 +93,7 @@ n_e    = (0.285*np.exp(-t**2)+0.015)*1e19    # Time dependent density of thermal
 T_e    = (0.040+0.760/(t+1)**2)*1e3          # Time dependent thermal temperature [eV]
 
 # Initial values in de Vries, changeable
-I_re  = 5.5e5           # Initial runaway current [A]
+I_re  = 6.5e5           # Initial runaway current [A]
 n_re0 = I_re/(e*c*A_c)  # Initial density of runaway electrons [m^-3]
 
 # Electric field calculated as in de Vries article
@@ -108,7 +118,10 @@ ds.eqsys.T_cold.setPrescribedData(T_e, times=t)
 ds.eqsys.n_cold.setPrescribedData(density=n_e, times=t)
 
 # Set ions
-ds.eqsys.n_i.addIon(name='D', Z=1, iontype=Ions.IONS_PRESCRIBED_FULLY_IONIZED, n=n)
+ds.eqsys.n_i.addIon(name='D', Z=2, iontype=Ions.IONS_PRESCRIBED_FULLY_IONIZED, n=n/2)
+#n=n säger hur många atomer. 1 atom med laddning 2 ger två elektroner så dela n med 2 om använder Z=2
+# Lägg till ännu en rad för Berrylium, byt namn på det.
+# Elektrondensitet som vi vill ha är n_e=n_D*Z_D+n_B*Z_B
 
 # Runaway electrons
 #ds.eqsys.n_re.setAvalanche(avalanche=Runaways.AVALANCHE_MODE_NEGLECT)
@@ -143,7 +156,6 @@ ds.radialgrid.setNr(Nr)
 
 # Set solver type
 ds.solver.setType(Solver.LINEAR_IMPLICIT) # semi-implicit time stepping
-ds.solver.preconditioner.setEnabled(False)
 
 # include otherquantities to save to output
 ds.other.include('fluid')
@@ -162,8 +174,8 @@ do = runiface(ds, 'output_de_Vries_test_2.h5', quiet=False)
 #### Manage result ####
 
 # Plot E_field
-do.eqsys.E_field.plot(r=[0,-1])
-plt.show()
+#do.eqsys.E_field.plot(r=[0,-1])
+#plt.show()
 
 # Plot I_p and I_re
 n_re = do.eqsys.n_re[:]
@@ -171,22 +183,24 @@ I_re = n_re*e*c*A_c
 tid=np.linspace(0,tMax,num=len(I_re))
 plt.plot(tid,I_re)
 do.eqsys.I_p.plot()
+#plt.xlim(-1.5, 8.5)
+#plt.ylim(0, 1e6)
 plt.show()
 
 # Plot n_tot and n_re
-do.eqsys.n_tot.plot(r=[0,-1])
-do.eqsys.n_re.plot(r=[0,-1])
-plt.show()
+#do.eqsys.n_tot.plot(r=[0,-1])
+#do.eqsys.n_re.plot(r=[0,-1])
+#plt.show()
 
 # Plot streaming parameter according to de Vries
-I_p = do.eqsys.I_p[:]
-T_J=1.602176565e-19*do.eqsys.T_cold[:] #Temperatur i joule
-v_th=np.sqrt(T_J/m_e)
-j_tot=I_p/A_c
-xi=np.abs(j_tot/(e*n*v_th))
-tid=np.linspace(0,tMax,num=len(xi))
-plt.plot(tid,xi)
-plt.show()
+#I_p = do.eqsys.I_p[:]
+#T_J=1.602176565e-19*do.eqsys.T_cold[:] #Temperatur i joule
+#v_th=np.sqrt(T_J/m_e)
+#j_tot=I_p/A_c
+#xi=np.abs(j_tot/(e*n*v_th))
+#tid=np.linspace(0,tMax,num=len(xi))
+#plt.plot(tid,xi)
+#plt.show()
 
 
 ###################################################################################################
