cc059163cfa9792a0e7bf0ce5c4f93e274d8ee3f
Phistian <79712626+Phistian@users.noreply.github.com>
2021-04-22 16:11:47 +0200

    Create setups.py
    All setup-kod för simulering 1 och 2 i funktionformat!

---
+++ setups.py
@@ -0,0 +1,79 @@
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+
+sys.path.append('../../py/')
+
+from DREAM.DREAMSettings import DREAMSettings
+from DREAM.DREAMOutput import DREAMOutput
+from DREAM import runiface
+import DREAM.Settings.Equations.IonSpecies as Ions
+import DREAM.Settings.Equations.RunawayElectrons as Runaways
+import DREAM.Settings.Solver as Solver
+import DREAM.Settings.CollisionHandler as Collisions
+import DREAM.Settings.TransportSettings as Transport
+import DREAM.Settings.Equations.ColdElectrons as ColdElectrons
+import DREAM.Settings.Equations.ColdElectronTemperature as ColdElectronTemperature
+import DREAM.Settings.Equations.ElectricField as ElectricField
+import DREAM.Settings.Equations.IonSpecies as Ions
+
+def setup1(E_initial, T_initial, V_loop_wall, t, Z_D, Z_B, n_D, n_B, B, a, r_0, Nr, tMax_c, Nt_c):
+
+    ds_c = DREAMSettings()
+    ds_c.collisions.collfreq_type = Collisions.COLLFREQ_TYPE_PARTIALLY_SCREENED #Är detta rätt?
+
+    ds_c.eqsys.E_field.setType(ElectricField.TYPE_SELFCONSISTENT)
+    ds_c.eqsys.E_field.setInitialProfile(efield=E_initial)
+    ds_c.eqsys.E_field.setBoundaryCondition(ElectricField.BC_TYPE_PRESCRIBED, V_loop_wall=V_loop_wall, times=t)
+
+    ds_c.eqsys.T_cold.setPrescribedData(T_initial, times=t)
+
+    ds_c.eqsys.n_cold.setType(ColdElectrons.TYPE_SELFCONSISTENT)
+
+    ds_c.eqsys.n_i.setIonization(Ions.IONIZATION_MODE_FLUID)
+    ds_c.eqsys.n_i.addIon(name='D', Z=Z_D, iontype=Ions.IONS_DYNAMIC, Z0=1, n=n_D)
+    ds_c.eqsys.n_i.addIon(name='B', Z=Z_B, iontype=Ions.IONS_DYNAMIC, Z0=3, n=n_B)
+
+    ds_c.eqsys.n_re.setAvalanche(avalanche=Runaways.AVALANCHE_MODE_FLUID_HESSLOW)
+
+    ds_c.eqsys.n_re.setDreicer(Runaways.DREICER_RATE_CONNOR_HASTIE)
+
+    ds_c.hottailgrid.setEnabled(False)
+    ds_c.runawaygrid.setEnabled(False)
+
+    ds_c.radialgrid.setB0(B)#, times=t)
+    ds_c.radialgrid.setMinorRadius(a)
+    ds_c.radialgrid.setMajorRadius(r_0)
+    ds_c.radialgrid.setWallRadius(a)
+    ds_c.radialgrid.setNr(Nr)
+
+    ds_c.solver.setType(Solver.NONLINEAR)
+
+    ds_c.timestep.setTmax(tMax_c)
+    ds_c.timestep.setNt(Nt_c)
+
+    ds_c.other.include('fluid')
+
+    return ds_c
+
+def setup2(ds_c, tMax, Nt, E_initial, T_initial, V_loop_wall, t, Z_D, Z_B, n_D, n_B, B, a, r_0, Nr, tMax_c, Nt_c):
+
+    ds = DREAMSettings(ds_c)
+    do_c = DREAMOutput('output_SELFCONSISTENT1' + '.h5')
+
+    ds.fromOutput('output_SELFCONSISTENT1' + '.h5')  # , ignore=['E_field','T_cold','n_cold','n_re'])
+
+
+    ds.eqsys.T_cold.setType(ColdElectronTemperature.TYPE_SELFCONSISTENT)
+    ds.eqsys.T_cold.setInitialProfile(T_initial)
+    ds.eqsys.n_cold.setType(ColdElectrons.TYPE_SELFCONSISTENT)
+    ds.collisions.collfreq_type = Collisions.COLLFREQ_TYPE_PARTIALLY_SCREENED
+
+    # ds.collisions.bremsstrahlung_mode = Collisions.BREMSSTRAHLUNG_MODE_STOPPING_POWER
+
+    ds.timestep.setTmax(tMax)
+    ds.timestep.setNt(Nt)
+
+
+    return ds
